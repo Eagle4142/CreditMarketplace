24d13bfb1c2d1553d362d260043f09023272281c
nergyzz <neryzz1@gmail.com>
2023-10-12 12:47:40 +0200

    -

--- Playground_Serhat.R
+++ Playground_Serhat.R
@@ -7,16 +7,22 @@ getwd()
 set.seed (1)
 
 #read .csv file
-creditData <- read_delim("/Users/serhat/Downloads/LCdata.csv", 
+creditDataSerhat <- read_delim("/Users/serhat/Downloads/LCdata.csv", 
                          delim = ";", escape_double = FALSE, trim_ws = TRUE)
 
 #display contents of imported data
-View(creditData)
+View(creditDataSerhat)
 
 #display structure of data
-str(creditData)
+str(creditDataSerhat)
 
 #get summary statistics of data
-summary(creditData)
+summary(creditDataSerhat)
 
-#new
+#deleting several columns with over 400'000 NA's
+creditDataSerhat <- subset(creditDataSerhat,select=-c(inq_last_12m,total_cu_tl,inq_fi,total_rev_hi_lim,all_util,
+                                                      max_bal_bc,open_rv_24m,open_rv_12m,il_util,total_bal_il,
+                                                      mths_since_rcnt_il,open_il_24m,open_il_12m,open_il_6m,
+                                                      open_acc_6m,dti_joint,annual_inc_joint,mths_since_last_major_derog,
+                                                      mths_since_last_record,mths_since_last_delinq))
+creditDataSerhat
--- main.R
+++ main.R
@@ -19,3 +19,13 @@ str(creditData)
 
 #get summary statistics of data
 summary(creditData)
+
+#deleting several columns with over 400'000 NA's (more than 50% -> see article below for reasoning)
+#https://towardsdatascience.com/7-ways-to-handle-missing-values-in-machine-learning-1a6326adf79e
+creditData2 <- subset(creditData,select=-c(inq_last_12m,total_cu_tl,inq_fi,total_rev_hi_lim,all_util,
+                                                      max_bal_bc,open_rv_24m,open_rv_12m,il_util,total_bal_il,
+                                                      mths_since_rcnt_il,open_il_24m,open_il_12m,open_il_6m,
+                                                      open_acc_6m,dti_joint,annual_inc_joint,mths_since_last_major_derog,
+                                                      mths_since_last_record,mths_since_last_delinq))
+
+creditData2
