a3d0e2106d3a13eb6fa0544efe8205b1cb14e740
nergyzz1@gmail.com <nergyzz1@gmail.com>
2023-10-12 10:32:25 +0200

    -

---
+++ Playground_Serhat.R
@@ -0,0 +1,20 @@
+library(readr)
+
+#check working directory
+getwd()
+
+#set seed to 1
+set.seed (1)
+
+#read .csv file
+creditData <- read_delim("/Users/serhat/Downloads/LCdata.csv", 
+                         delim = ";", escape_double = FALSE, trim_ws = TRUE)
+
+#display contents of imported data
+View(creditData)
+
+#display structure of data
+str(creditData)
+
+#get summary statistics of data
+summary(creditData)
