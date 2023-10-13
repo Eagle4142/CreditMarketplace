8a28d084e215ff675482ae9e457ffa0b5564a2e3
nergyzz <neryzz1@gmail.com>
2023-10-13 09:25:35 +0200

    -

--- main.R
+++ main.R
@@ -8,7 +8,7 @@ getwd()
 set.seed (1)
 
 #read .csv file
-creditData <- read_delim("LCdata.csv", 
+creditData <- read_delim("/Users/serhat/Downloads/LCdata.csv", 
                      delim = ";", escape_double = FALSE, trim_ws = TRUE)
 
 #display contents of imported data
@@ -27,11 +27,11 @@ creditData2 <- subset(creditData,select=-c(inq_last_12m,total_cu_tl,inq_fi,total
                                                       mths_since_rcnt_il,open_il_24m,open_il_12m,open_il_6m,
                                                       open_acc_6m,dti_joint,annual_inc_joint,mths_since_last_major_derog,
                                                       mths_since_last_record,mths_since_last_delinq))
+summary(creditData2)
 
 creditData3 <- subset(creditData2,select=-c(collection_recovery_fee,installment,issue_d,last_pymnt_amnt,
                                                       last_pymnt_d,loan_status,next_pymnt_d,out_prncp,out_prncp_inv,
                                                       pymnt_plan,recoveries,term,total_pymnt,total_pymnt_inv,total_rec_int,
                                                       total_rec_late_fee,total_rec_prncp))
 
-summary(creditData2)
-cor(creditData)
+summary(creditData3)
